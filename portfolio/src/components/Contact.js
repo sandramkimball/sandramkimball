9b22be884f5d08e8ef91a8f51989a4e1f0837056
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-22 17:18:48 -0800

    initial commit

---
+++ portfolio/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default LeftBar = () => {
+    return (
+        <section id='home' class="banner">
+                <div>
+                    <img src='../images/profile-bw.jpg' alt='black&white profile of a girl smiling'/>
+                    <h1>SANDRA KIMBALL</h1>
+                    <h2>Full Stack Web Developer</h2>
+                </div>
+                <script>
+                    {/* let homebase = document.querySelectorAll('.banner div');
+                    TweenLite.from(homebase, 4, {opacity: 0, x: -50}); */}
+                </script>
+            </section>
+    )
+}
