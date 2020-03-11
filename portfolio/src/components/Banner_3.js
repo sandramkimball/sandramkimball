378348465ae112b64de817fd06346902b5ad8680
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-10 18:37:21 -0700

    added links and desc to project slides

--- portfolio/src/components/Banner_3.js
+++ portfolio/src/components/Banner_3.js
@@ -9,10 +9,34 @@ import sautidb3 from '../images/sautidb3.png';
 import stylist from '../images/stylist-find.png';
 
 const slides = [
-        ({style}) => <animated.img src={stylist} style={{...style, height: '60vh', cursor: 'pointer'}}></animated.img>,
-        ({style}) => <animated.img src={homepage} style={{...style, height: '80vh', cursor: 'pointer'}}></animated.img>,
-        ({style}) => <animated.img src={icnst} style={{...style, height: '60vh', cursor: 'pointer'}}></animated.img>,
-        ({style}) => <animated.img src={sautidb3} style={{...style, height: '60vh', cursor: 'pointer'}}></animated.img>,
+        ({style}) => <animated.div style={{...style, cursor: 'pointer'}}>
+                    <img src={homepage}/>
+                    <a href='https://foodie-fun-app.netlify.com/?_ga=2.228749211.602302675.1567105491-253791327.1567105491' target="_blank">
+                        <h2>Foodie Fun UI</h2>
+                        <p>Collaborated with a UX designer to build a marketing site for a food blog app.</p>
+                    </a>
+                    </animated.div>,
+        ({style}) => <animated.div style={{...style, cursor: 'pointer'}}>
+                    <img src={icnst}/>
+                    <a href='https://icnst-development.netlify.com/' target="_blank">   
+                        <h2>Child Nutrition Tracker</h2>
+                        <p>Worked in collabe with a senior React developer to build out components, forms, routes for an application designed to help doctors in foreign nations log and chart health information for patients.</p>
+                    </a>
+                    </animated.div>,
+        ({style}) => <animated.div style={{...style,cursor: 'pointer'}}>
+                    <img src={sautidb3}/>
+                    <a href='https://sauti.now.sh/' target="_blank">
+                        <h2>Sauti Traders</h2>
+                        <p>Worked on a team of five with a non-profit organization to deliver a cross-filtration search platform for traders data collected by the organization.</p>
+                    </a>
+                    </animated.div>,
+        ({style}) => <animated.div style={{...style, cursor: 'pointer'}}>
+                    <img src={stylist}/>
+                    <a href='https://stylistfind.now.sh/' target="_blank">
+                        <h2>Stylist Find</h2>
+                        <p>Built a SQL database, populated it with dummy data, and used it in a front end application designed as a professional platform for hairstylists to self promote.</p>
+                    </a>
+                    </animated.div>,
 ]
 
 const Projects = () => {
@@ -34,48 +58,6 @@ const Projects = () => {
                     return <Slide key={key} style={props}/>
                 })}
             </div>
-            
-            {/* <div class='projects-container'>
-                <Project>
-                    <a href='https://foodie-fun-app.netlify.com/?_ga=2.228749211.602302675.1567105491-253791327.1567105491' target="_blank">
-                        <img src={homepage}/>
-                        <div>
-                            <h2>Foodie Fun UI</h2>
-                            <p>Collaborated with a UX designer to build a marketing site for a food blog app.</p>
-                        </div>
-                    </a>
-                </Project>
-
-                <Project>
-                    <a href='https://icnst-development.netlify.com/' target="_blank">
-                        <img src={icnst}/>
-                        <div>
-                            <h2>Child Nutrition Tracker</h2>
-                            <p>Worked in collab with a senior React developer to build half the components, forms and routes to an application designed to help volunteer doctors in foreign nations to log and chart health information for patients.</p>
-                        </div>
-                    </a>
-                </Project>
-
-                <Project>
-                    <a href='https://sauti.now.sh/' target="_blank">
-                        <img src={sautidb3}/>
-                        <div>
-                            <h2>Sauti Traders</h2>
-                            <p>Worked on a team of five with a non-profit organization to deliver a cross-filtration search platform for traders data collected by the organization.</p>
-                        </div>
-                    </a>
-                </Project>
-
-                <Project>
-                    <a href='https://stylistfind.now.sh/' target="_blank">
-                        <img src={stylist}/>
-                        <div>
-                            <h2>Stylist Find</h2>
-                            <p>Built a SQL database, populated it with dummy data, and used it in a front end application designed as a professional platform for hairstylists to self promote.</p>
-                        </div>
-                    </a>
-                </Project>
-            </div> */}
         </ProjectsContainer>
     )
 }
@@ -84,8 +66,8 @@ export default Projects;
 
 const ProjectsContainer = styled.section`
     width: 100vw;
-    height: 90vh;
-    margin: 0 auto;
+    height: 100vh;
+    margin: auto;
     display: flex;
     justify-content: center; 
     background: #f7f7f7;  
@@ -93,9 +75,20 @@ const ProjectsContainer = styled.section`
     will-change: transform, opacity;
     .projects-container{
         margin: auto;
-        display: flex; 
         justify-content: center;  
-        width: 90%;   
+        height: 80vh; 
+    }
+    img{
+        height: 60vh;
+    }
+    a{ 
+        font-family: 'Roboto', sans-serif;        
+        transform: none;
+        text-decoration: none;
+        align-items: center;
+        justify-content: center;
+        color: black;
+        :hover{color: gray}
     }
 `;
 
@@ -110,12 +103,7 @@ const Project = styled.div`
         text-decoration: none;
         align-items: center;
         justify-content: center;
-        img{
-            width: 100%;
-            height: 50%;
-            object-fit: cover;
-            object-position: top;
-        }
+        
         div{
             color: #000;
             width: 90%;
