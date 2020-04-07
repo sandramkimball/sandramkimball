e86129a58e118e9b1f0062eb6c333440cdef43ba
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-04-07 09:49:25 -0700

    added chai test and moved proje ct slides to own file

---
+++ portfolio/src/components/ProjectSlides.js
@@ -0,0 +1,75 @@
+
+//images
+import homepage from '../images/tinyHomepage.png';
+import icnst from '../images/icnst.jpg';
+import sautidb3 from '../images/sautidb3.png';
+import stylist from '../images/stylist-find.png';
+import mars from '../images/mars-explor.png';
+import toDo from '../images/reducer-todo.png'
+
+export default ProjectSlides = [
+    {
+        id: 1,
+        index: 0,
+        href: 'https://foodie-fun-app.netlify.com/?_ga=2.228749211.602302675.1567105491-253791327.1567105491',
+        img: homepage,
+        title: 'FOODIE FUN',
+        text: 'Collaborated with a UX designer to build a marketing site for a food blog app.'
+    },
+    {
+        id: 2, 
+        index: 1,
+        href: 'https://icnst-development.netlify.com/',
+        img: icnst,
+        title: 'CHILD NUTRITION TRACKER',
+        text: 'Collaborated with a senior React developer for an application to help doctors in foreign nations log and chart health information for patients.'
+    },
+    {
+        id: 3,
+        index: 2,
+        href: 'https://sauti.now.sh/',
+        img: sautidb3,
+        title: 'SAUTI TRADERS',
+        text: 'Worked on a team of five with a non-profit organization to deliver a cross-filtration search platform for traders data collected by the organization.'
+    },
+    {
+        id: 4,
+        index: 3,
+        href: 'https://stylistfind.now.sh/',
+        img: stylist,
+        title: 'STYLIST FIND',
+        text: 'Built a Node database and React front end application designed as a professional platform for hairstylists to self promote.'
+    },
+    {
+        id: 5,
+        index: 4,
+        href: 'https://reducer-todo.sandramkimball.now.sh/',
+        img: toDo,
+        title: 'TO DO LIST',
+        text: 'A simple to do list where you can add and delete items using Redux.'
+    },
+    {
+        id: 6,
+        index: 5,
+        href: null,
+        img: mars,
+        title: 'COMING SOON',
+        text: 'Online Python/Django adventure game.'
+    },
+    {
+        id: 7,
+        index: 0,
+        href: 'https://foodie-fun-app.netlify.com/?_ga=2.228749211.602302675.1567105491-253791327.1567105491',
+        img: homepage,
+        title: 'FOODIE FUN',
+        text: 'Collaborated with a UX designer to build a marketing site for a food blog app.'
+    },
+    {
+        id: 8, 
+        index: 1,
+        href: 'https://icnst-development.netlify.com/',
+        img: icnst,
+        title: 'CHILD NUTRITION TRACKER',
+        text: 'Collaborated with a senior React developer for an application to help doctors in foreign nations log and chart health information for patients.'
+    },
+]
