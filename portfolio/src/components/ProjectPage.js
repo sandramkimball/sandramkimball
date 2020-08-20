2219bd3c95b47c44a68c3b90854faf3a666674ba
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-08-19 20:44:19 -0700

    adjusted links on project pg

--- portfolio/src/components/ProjectPage.js
+++ portfolio/src/components/ProjectPage.js
@@ -2,50 +2,52 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Card from './Card'
 import projects from './Projects'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { fab, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faGithub,  } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProjectsPage = () => {     
     const [bigProj, setBigProj] = useState(projects[2])
-
-    function handleClick(obj) {
+    const handleClick = (obj) => {
         setBigProj(obj)
     }
     
     return (
         <ProjectsContainer id='3'> 
-            <section className='pj-big'>
+            <section className='pj-big' name='pj-big'>
                 <img src={bigProj.img}/> 
-                <a href={bigProj.href} target="_blank">  
-                    <div>
-                        <h2>{bigProj.title}</h2>
-                        <p className='stack'>{bigProj.stack}</p>
-                        <p>{bigProj.text}</p>
-                    </div>
-                    <div className='git-link'>                
-                    {bigProj.f_url !== null && (
-                        <a href={bigProj.f_url} target="_blank">
-                            <FontAwesomeIcon icon={faGithub} />
-                            <h5>FE</h5>
-                        </a>
-                    )}
-                    
-                    {bigProj.b_url !== null && (<a href={bigProj.b_url} target="_blank">
-                        <FontAwesomeIcon icon={faGithub} />
-                        <h5>BE</h5>
-                    </a>)}
-                </div>
-                </a>
                 
+                <div>
+                    <h2>
+                        {bigProj.title} 
+                        <a href={bigProj.href} target="_blank"> <FontAwesomeIcon icon={faChevronRight} /></a>
+                    </h2>
+                    <p className='stack'>{bigProj.stack}</p>
+                    <p>{bigProj.text}</p>
+                
+                    <div className='git-link'>                
+                        {bigProj.f_url !== null && (
+                            <a href={bigProj.f_url} target="_blank">
+                                <FontAwesomeIcon icon={faGithub} />
+                                <h5>FE</h5>
+                            </a>
+                        )}
+                        {bigProj.b_url !== null && (<a href={bigProj.b_url} target="_blank">
+                            <FontAwesomeIcon icon={faGithub} />
+                            <h5>BE</h5>
+                        </a>)}
+                        
+                    </div>
+                </div>
             </section>
 
-            <section className='pj-container' data-testid='pj-container' >
+            <section className='pj-container' data-testid='pj-container'>
                 {projects.map( obj => 
                     <span onClick={ () => handleClick(obj)} >
                         <Card 
                             key={obj.id} 
                             obj={obj} 
-                        /> 
+                        />
                     </span>
                 )}
             </section>
@@ -60,18 +62,6 @@ const ProjectsContainer = styled.section`
     margin-top: 5vh;
     padding: 2vh 0;
     background: #e7e7e7;  
-    a{ 
-        font-family: 'Roboto', sans-serif;        
-        color: #000;
-        align-items: center;
-        justify-content: center;
-        margin: auto;
-        :hover{color: #007bff}
-    }    
-    span{
-        height: 100%;
-        padding: 10px 0;
-    }
 
     .pj-container{
         margin: auto;
@@ -81,6 +71,18 @@ const ProjectsContainer = styled.section`
         height: 100%; 
         width: 65vw;
         justify-content: space-between; 
+        span{
+            height: 100%;
+            padding: 10px 0;
+        }
+        a{ 
+            font-family: 'Roboto', sans-serif;        
+            color: #000;
+            align-items: center;
+            justify-content: center;
+            margin: auto;
+            :hover{color: #007bff}
+        }   
     }
 
     .pj-big{
@@ -91,19 +93,20 @@ const ProjectsContainer = styled.section`
         justify-content: space-evenly;
         background: #fff;
         margin: 5vh auto;
-        padding: 2vh;
+        padding: 2vh; 
         img{
-            height: 40vh;
-            width: 50%;
+            height: 90%;
+            width: 48%;
             object-fit: cover;
         }
         div{
             text-align: left;
-            width: 90%;
+            width: 48%;
             margin: auto;
         }
         .git-link{
             display: flex;
+            width: 100%;
             h5{
                 font-size: 1rem; 
                 color: gray;
@@ -112,17 +115,17 @@ const ProjectsContainer = styled.section`
             svg{
                 font-size: 1.5rem;
                 color: gray;
-                padding: 0 2px;
+                padding: 0 2px 0 0;
                 margin: 0;
             }
             a{
                 display: flex;
                 align-content: center;
-                margin: 0 5px;
+                text-decoration: none;
+                margin: 0 5px 0 0;
+                :hover{
+                    svg, h5{background: none; color: #007bff}
+                }
             }
-            a:hover{
-                svg, h5{background: none; color: #007bff}
-                
-        }
     }
 `;
